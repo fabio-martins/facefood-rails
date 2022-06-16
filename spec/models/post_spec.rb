88927edd9fa9012cec9f31c2a3ee4b497319e647
Fabio <martinsfabiojr@gmail.com>
2022-06-16 11:19:50 -0300

    feat: add comments relationship to posts

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -4,4 +4,9 @@ RSpec.describe Post, type: :model do
   context "validations" do
     it { is_expected.to validate_presence_of(:content) }
   end
+
+  context "associations" do
+    it { is_expected.to have_many(:comments) }
+  end
+  
 end
