bcbfb0fa04079f8d1dc34592c868d6ec827a2a66
Fabio <martinsfabiojr@gmail.com>
2022-06-16 11:07:15 -0300

    feat: add post relationship with homepage

--- app/models/homepage.rb
+++ app/models/homepage.rb
@@ -1,3 +1,4 @@
 class Homepage < ApplicationRecord
   belongs_to :user
+  has_many :posts
 end
--- spec/models/homepage_spec.rb
+++ spec/models/homepage_spec.rb
@@ -2,6 +2,6 @@ require 'rails_helper'
 
 RSpec.describe Homepage, type: :model do
   context "validations" do
-    # it { is_expected.to have_many(:post) }
+    it { is_expected.to have_many(:posts) }
   end  
 end
