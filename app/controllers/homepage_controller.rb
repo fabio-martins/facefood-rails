5921e1f098375e151b3a614f5413d7bc87d10f53
Fabio <martinsfabiojr@gmail.com>
2022-06-16 17:58:37 -0300

    feat: set fake user to session and use in view

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -1,4 +1,13 @@
 class HomepageController < ApplicationController
+  before_action :set_logged_user
+
   def index
+    @posts = Post.all
+  end
+
+  def set_logged_user
+    if session[:current_user].empty?
+      session[:current_user] = User.all.sample
+    end  
   end
 end
