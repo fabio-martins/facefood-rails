fc6253e38f09c98e24004956300e6d1bf38a8f64
Fabio <martinsfabiojr@gmail.com>
2022-06-16 11:14:07 -0300

    fix: add null false to important info

--- db/migrate/20220616135914_create_posts.rb
+++ db/migrate/20220616135914_create_posts.rb
@@ -2,7 +2,7 @@ class CreatePosts < ActiveRecord::Migration[7.0]
   def change
     create_table :posts do |t|
       t.references :homepage, null: false, foreign_key: true
-      t.string :content
+      t.string :content, null: false
 
       t.timestamps
     end
