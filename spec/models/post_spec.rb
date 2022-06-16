070b04c6ea7bab27ee4bae642ddefec8f387bd9c
Fabio <martinsfabiojr@gmail.com>
2022-06-16 11:14:44 -0300

    refactor: add context to tests

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -1,5 +1,7 @@
 require 'rails_helper'
 
 RSpec.describe Post, type: :model do
-  it { is_expected.to validate_presence_of(:content) }
+  context "validations" do
+    it { is_expected.to validate_presence_of(:content) }
+  end
 end
