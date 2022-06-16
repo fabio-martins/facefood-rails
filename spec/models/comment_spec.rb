d98f596a2422197398fb565d5bbd920b164aafc5
Fabio <martinsfabiojr@gmail.com>
2022-06-16 17:13:20 -0300

    test: add missing validates

--- spec/models/comment_spec.rb
+++ spec/models/comment_spec.rb
@@ -3,6 +3,12 @@ require 'rails_helper'
 RSpec.describe Comment, type: :model do
   context "validations" do
     it { is_expected.to validate_presence_of(:content) }
+    it { is_expected.to validate_presence_of(:user) }
+    it { is_expected.to validate_presence_of(:post) }
   end
-  
+
+  context "associations" do
+    it{ is_expected.to belong_to(:user) }
+    it{ is_expected.to belong_to(:post) }
+  end
 end
--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -3,10 +3,12 @@ require 'rails_helper'
 RSpec.describe Post, type: :model do
   context "validations" do
     it { is_expected.to validate_presence_of(:content) }
+    it { is_expected.to validate_presence_of(:user) }
   end
 
   context "associations" do
     it { is_expected.to have_many(:comments) }
+    it { is_expected.to belong_to(:user) }
   end
   
 end
