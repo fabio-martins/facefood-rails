7eede14e67aa7d82d779098e74036e9d79ed1688
Fabio <martinsfabiojr@gmail.com>
2022-06-16 10:53:47 -0300

    feat: add homepage use case

---
+++ spec/factories/homepages.rb
@@ -0,0 +1,5 @@
+FactoryBot.define do
+  factory :homepage do
+    
+  end
+end
---
+++ spec/models/homepage_spec.rb
@@ -0,0 +1,7 @@
+require 'rails_helper'
+
+RSpec.describe Homepage, type: :model do
+  context "validations" do
+    # it { is_expected.to have_many(:post) }
+  end  
+end
