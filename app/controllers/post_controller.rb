d5824c4a4dfd48b365bc376c4a8b9bcf3fd4a58b
Fabio <martinsfabiojr@gmail.com>
2022-06-17 18:25:12 -0300

    refactor: correct fixed user

--- app/controllers/post_controller.rb
+++ app/controllers/post_controller.rb
@@ -1,7 +1,7 @@
 class PostController < ApplicationController
   def create
     post = Post.new(params_post)
-    post.user_id = 1
+    post.user_id = session[:current_user]["id"]
     post.save!
     post.broadcast_prepend_to( post, partial: "posts/list" )
     redirect_to root_path
