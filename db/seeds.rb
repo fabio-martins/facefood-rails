ae52b5dffcd629ce14f832fcef4b6e23c6d95684
Fabio <martinsfabiojr@gmail.com>
2022-06-17 18:25:34 -0300

    chore: cofigure fixed user to app

--- db/seeds.rb
+++ db/seeds.rb
@@ -8,16 +8,31 @@
 
 require 'faker'
 
-10.times do
-    User.create(full_name: Faker::Name.name)
-end
+user_1 = Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, 'app/assets/images/photos/1.jpeg')))
+User.create(full_name: 'Rebeca J. Stuart', photo: user_1)
 
-User.all.each do |user|
-    5.times do
-        Post.create(user: user, content: Faker::Lorem.paragraph(sentence_count: 10))
-    end
-end
+user_2 = Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, 'app/assets/images/photos/2.png')))
+User.create(full_name: 'Erica C. Flower', photo: user_2)
 
-Post.all.each do |post|
-    Comment.create(user: User.all.sample,post: post, content: Faker::Lorem.sentence)
-end
+user_3 = Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, 'app/assets/images/photos/3.png')))
+User.create(full_name: 'Jhon Mayers', photo: user_3)
+
+user_4 = Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, 'app/assets/images/photos/4.png')))
+User.create(full_name: 'Mike Mellon', photo: user_4)
+
+user_5 = Rack::Test::UploadedFile.new(File.open(File.join(Rails.root, 'app/assets/images/photos/5.jpg')))
+User.create(full_name: 'Fabio Martins', photo: user_5)
+
+# 10.times do
+#     User.create(full_name: Faker::Name.name)
+# end
+
+# User.all.each do |user|
+#     5.times do
+#         Post.create(user: user, content: Faker::Lorem.paragraph(sentence_count: 10))
+#     end
+# end
+
+# Post.all.each do |post|
+#     Comment.create(user: User.all.sample,post: post, content: Faker::Lorem.sentence)
+# end
