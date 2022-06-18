c6369757753e215c4e11a36f9839fe3dfff8ef20
Fabio <martinsfabiojr@gmail.com>
2022-06-18 09:21:18 -0300

    feat: add posts search

--- app/controllers/homepage_controller.rb
+++ app/controllers/homepage_controller.rb
@@ -2,7 +2,11 @@ class HomepageController < ApplicationController
   before_action :set_logged_user
 
   def index
-    @posts = Post.all.order(created_at: :desc)
+    if params[:query].present?
+      @posts = Post.where('content ilike ?', "%#{params[:query]}%")
+    else  
+      @posts = Post.all.order(created_at: :desc)
+    end  
   end
 
   def set_logged_user
