42c390603b76fa15d79e75fa1fa563add774cbe8
Fabio <martinsfabiojr@gmail.com>
2022-06-19 22:50:42 -0300

    test: add test show profile in home

--- app/helpers/homepage_helper.rb
+++ app/helpers/homepage_helper.rb
@@ -1,6 +1,7 @@
 module HomepageHelper
     def get_photo_from_current_user
-        current_user = session[:current_user]
-        User.find(current_user["id"]).photo unless current_user.nil?
+        user_id = session[:current_user]["id"]
+        current_user = User.find(user_id)
+        current_user.photo unless current_user.photo.nil?
     end
 end
--- spec/features/homepage_spec.rb
+++ spec/features/homepage_spec.rb
@@ -1,7 +1,16 @@
 require 'rails_helper'
 
 RSpec.feature "Homepage", type: :feature do
+  given!(:user) { create(:user, full_name: 'New User') }
+  
   scenario "show welcome message" do
     visit root_path
-  end  
+    expect(page).to have_css('.input-new-post')
+  end
+
+  scenario "logged in user access profile page" do
+    page.set_rack_session(current_user: user.id)
+    visit root_path
+    expect(page).to hava_content('New User')
+  end
 end
